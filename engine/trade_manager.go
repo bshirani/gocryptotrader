aa6fd205dfcf503c00a0a5bb4e3790bb187babb2
Bijan Pourriahi <2478512-sub_conscious@users.noreply.gitlab.com>
2021-10-10 22:51:45 -0500

    update

--- engine/trade_manager.go
+++ engine/trade_manager.go
@@ -17,10 +17,7 @@ import (
 	"gocryptotrader/currency"
 	"gocryptotrader/data"
 	"gocryptotrader/data/kline"
-	"gocryptotrader/data/kline/api"
 	"gocryptotrader/data/kline/csv"
-	"gocryptotrader/data/kline/database"
-	"gocryptotrader/data/kline/live"
 	gctdatabase "gocryptotrader/database"
 	"gocryptotrader/eventtypes"
 	"gocryptotrader/eventtypes/fill"
@@ -268,6 +265,46 @@ dataLoadingIssue:
 	return nil
 }
 
+func (tm *TradeManager) runLive() error {
+	processEventTicker := time.NewTicker(time.Second)
+	for {
+		select {
+		case <-tm.shutdown:
+			return nil
+		case <-processEventTicker.C:
+			for ev := tm.EventQueue.NextEvent(); ; ev = tm.EventQueue.NextEvent() {
+				if ev == nil {
+					dataHandlerMap := tm.Datas.GetAllData()
+					for exchangeName, exchangeMap := range dataHandlerMap {
+						for assetItem, assetMap := range exchangeMap {
+							for currencyPair, dataHandler := range assetMap {
+								d := dataHandler.Next()
+								if d == nil {
+									if !tm.hasHandledEvent {
+										log.Errorf(log.TradeManager, "Unable to perform `Next` for %v %v %v", exchangeName, assetItem, currencyPair)
+									}
+								}
+								tm.EventQueue.AppendEvent(d)
+							}
+						}
+					}
+				}
+
+				if ev != nil {
+					err := tm.handleEvent(ev)
+					if err != nil {
+						return err
+					}
+				}
+				if !tm.hasHandledEvent {
+					tm.hasHandledEvent = true
+				}
+			}
+		}
+	}
+	return nil
+}
+
 // LIVE FUNCTIONALITY
 func (tm *TradeManager) Start() error {
 	// throw error if not live
@@ -490,6 +527,7 @@ func (tm *TradeManager) loadExchangePairAssetBase(exch, base, quote, ass string)
 	return e, fPair, a, nil
 }
 
+// this is only needed in backtest mode, except for when live runs the catchup process
 // setupBot sets up a basic bot to retrieve exchange data
 // as well as process orders
 // setup order manager, exchange manager, database manager
@@ -714,132 +752,6 @@ func (tm *TradeManager) loadData(cfg *config.Config, exch gctexchange.IBotExchan
 	return resp, nil
 }
 
-func loadDatabaseData(cfg *config.Config, name string, fPair currency.Pair, a asset.Item, dataType int64) (*kline.DataFromKline, error) {
-	if cfg == nil || cfg.DataSettings.DatabaseData == nil {
-		return nil, errors.New("nil config data received")
-	}
-	if cfg.DataSettings.Interval <= 0 {
-		return nil, errIntervalUnset
-	}
-
-	return database.LoadData(
-		cfg.DataSettings.DatabaseData.StartDate,
-		cfg.DataSettings.DatabaseData.EndDate,
-		cfg.DataSettings.Interval,
-		strings.ToLower(name),
-		dataType,
-		fPair,
-		a)
-}
-
-func loadAPIData(cfg *config.Config, exch gctexchange.IBotExchange, fPair currency.Pair, a asset.Item, resultLimit uint32, dataType int64) (*kline.DataFromKline, error) {
-	if cfg.DataSettings.Interval <= 0 {
-		return nil, errIntervalUnset
-	}
-	dates, err := gctkline.CalculateCandleDateRanges(
-		cfg.DataSettings.APIData.StartDate,
-		cfg.DataSettings.APIData.EndDate,
-		gctkline.Interval(cfg.DataSettings.Interval),
-		resultLimit)
-	if err != nil {
-		return nil, err
-	}
-	candles, err := api.LoadData(context.TODO(),
-		dataType,
-		cfg.DataSettings.APIData.StartDate,
-		cfg.DataSettings.APIData.EndDate,
-		cfg.DataSettings.Interval,
-		exch,
-		fPair,
-		a)
-	if err != nil {
-		return nil, fmt.Errorf("%v. Please check your GoCryptoTrader configuration", err)
-	}
-	dates.SetHasDataFromCandles(candles.Candles)
-	summary := dates.DataSummary(false)
-	if len(summary) > 0 {
-		log.Warnf(log.TradeManager, "%v", summary)
-	}
-	candles.FillMissingDataWithEmptyEntries(dates)
-	candles.RemoveOutsideRange(cfg.DataSettings.APIData.StartDate, cfg.DataSettings.APIData.EndDate)
-	return &kline.DataFromKline{
-		Item:        *candles,
-		RangeHolder: dates,
-	}, nil
-}
-
-func loadLiveData(cfg *config.Config, base *gctexchange.Base) error {
-	if cfg == nil || base == nil || cfg.DataSettings.LiveData == nil {
-		return eventtypes.ErrNilArguments
-	}
-	if cfg.DataSettings.Interval <= 0 {
-		return errIntervalUnset
-	}
-
-	if cfg.DataSettings.LiveData.APIKeyOverride != "" {
-		base.API.Credentials.Key = cfg.DataSettings.LiveData.APIKeyOverride
-	}
-	if cfg.DataSettings.LiveData.APISecretOverride != "" {
-		base.API.Credentials.Secret = cfg.DataSettings.LiveData.APISecretOverride
-	}
-	if cfg.DataSettings.LiveData.APIClientIDOverride != "" {
-		base.API.Credentials.ClientID = cfg.DataSettings.LiveData.APIClientIDOverride
-	}
-	if cfg.DataSettings.LiveData.API2FAOverride != "" {
-		base.API.Credentials.PEMKey = cfg.DataSettings.LiveData.API2FAOverride
-	}
-	if cfg.DataSettings.LiveData.APISubAccountOverride != "" {
-		base.API.Credentials.Subaccount = cfg.DataSettings.LiveData.APISubAccountOverride
-	}
-	validated := base.ValidateAPICredentials()
-	base.API.AuthenticatedSupport = validated
-	if !validated && cfg.DataSettings.LiveData.RealOrders {
-		log.Warn(log.TradeManager, "invalid API credentials set, real orders set to false")
-		cfg.DataSettings.LiveData.RealOrders = false
-	}
-	return nil
-}
-
-func (tm *TradeManager) runLive() error {
-	processEventTicker := time.NewTicker(time.Second)
-	for {
-		select {
-		case <-tm.shutdown:
-			return nil
-		case <-processEventTicker.C:
-			for ev := tm.EventQueue.NextEvent(); ; ev = tm.EventQueue.NextEvent() {
-				if ev == nil {
-					dataHandlerMap := tm.Datas.GetAllData()
-					for exchangeName, exchangeMap := range dataHandlerMap {
-						for assetItem, assetMap := range exchangeMap {
-							for currencyPair, dataHandler := range assetMap {
-								d := dataHandler.Next()
-								if d == nil {
-									if !tm.hasHandledEvent {
-										log.Errorf(log.TradeManager, "Unable to perform `Next` for %v %v %v", exchangeName, assetItem, currencyPair)
-									}
-								}
-								tm.EventQueue.AppendEvent(d)
-							}
-						}
-					}
-				}
-
-				if ev != nil {
-					err := tm.handleEvent(ev)
-					if err != nil {
-						return err
-					}
-				}
-				if !tm.hasHandledEvent {
-					tm.hasHandledEvent = true
-				}
-			}
-		}
-	}
-	return nil
-}
-
 // -----------------------------------
 // DATA PROCESSING
 // -----------------------------------
@@ -868,7 +780,6 @@ func (tm *TradeManager) handleEvent(ev eventtypes.EventHandler) error {
 }
 
 func (tm *TradeManager) processSingleDataEvent(ev eventtypes.DataEventHandler) error {
-
 	err := tm.updateStatsForDataEvent(ev)
 	if err != nil {
 		return err
@@ -992,104 +903,4 @@ func (tm *TradeManager) processFillEvent(ev fill.Event) {
 		log.Error(log.TradeManager, err)
 		return
 	}
-}
-
-// ---------------------------
-// DATA LOADING
-// ---------------------------
-// loadLiveDataLoop is an incomplete function to continuously retrieve exchange data on a loop
-// from live. Its purpose is to be able to perform strategy analysis against current data
-func (tm *TradeManager) loadLiveDataLoop(resp *kline.DataFromKline, cfg *config.Config, exch gctexchange.IBotExchange, fPair currency.Pair, a asset.Item, dataType int64) {
-	startDate := time.Now().Add(-cfg.DataSettings.Interval * 2)
-	dates, err := gctkline.CalculateCandleDateRanges(
-		startDate,
-		startDate.AddDate(1, 0, 0),
-		gctkline.Interval(cfg.DataSettings.Interval),
-		0)
-	if err != nil {
-		log.Errorf(log.TradeManager, "%v. Please check your GoCryptoTrader configuration", err)
-		return
-	}
-	candles, err := live.LoadData(context.TODO(),
-		exch,
-		dataType,
-		cfg.DataSettings.Interval,
-		fPair,
-		a)
-	if err != nil {
-		log.Errorf(log.TradeManager, "%v. Please check your GoCryptoTrader configuration", err)
-		return
-	}
-	dates.SetHasDataFromCandles(candles.Candles)
-	resp.RangeHolder = dates
-	resp.Item = *candles
-
-	loadNewDataTimer := time.NewTimer(time.Second * 5)
-	for {
-		select {
-		case <-tm.shutdown:
-			return
-		case <-loadNewDataTimer.C:
-			log.Infof(log.TradeManager, "fetching data for %v %v %v %v", exch.GetName(), a, fPair, cfg.DataSettings.Interval)
-			loadNewDataTimer.Reset(time.Second * 15)
-			err = tm.loadLiveData(resp, cfg, exch, fPair, a, dataType)
-			if err != nil {
-				log.Error(log.TradeManager, err)
-				return
-			}
-		}
-	}
-}
-
-func (tm *TradeManager) loadLiveData(resp *kline.DataFromKline, cfg *config.Config, exch gctexchange.IBotExchange, fPair currency.Pair, a asset.Item, dataType int64) error {
-	if resp == nil {
-		return errNilData
-	}
-	if cfg == nil {
-		return errNilConfig
-	}
-	if exch == nil {
-		return errNilExchange
-	}
-	candles, err := live.LoadData(context.TODO(),
-		exch,
-		dataType,
-		cfg.DataSettings.Interval,
-		fPair,
-		a)
-	if err != nil {
-		return err
-	}
-	if len(candles.Candles) == 0 {
-		return nil
-	}
-	resp.AppendResults(candles)
-	tm.Reports.UpdateItem(&resp.Item)
-	log.Info(log.TradeManager, "sleeping for 30 seconds before checking for new candle data")
-	return nil
-}
-
-func (b *TradeManager) heartBeat() {
-	for range time.Tick(time.Second * 15) {
-		log.Info(log.TradeManager, "heartbeat")
-	}
-}
-
-// updateStatsForDataEvent makes various systems aware of price movements from
-// data events
-func (tm *TradeManager) updateStatsForDataEvent(ev eventtypes.DataEventHandler) error {
-	// update statistics with the latest price
-	err := tm.Statistic.SetupEventForTime(ev)
-	if err != nil {
-		if err == statistics.ErrAlreadyProcessed {
-			return err
-		}
-		log.Error(log.TradeManager, err)
-	}
-	// update portfolio manager with the latest price
-	err = tm.Portfolio.UpdateHoldings(ev)
-	if err != nil {
-		log.Error(log.TradeManager, err)
-	}
-	return nil
 }
