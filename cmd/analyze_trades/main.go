e16799a4d3a21e3737c26d0edf0d1c4ca1f27ae3
Bijan Pourriahi <2478512-sub_conscious@users.noreply.gitlab.com>
2021-10-27 14:53:16 -0500

    update

--- cmd/analyze_trades/main.go
+++ cmd/analyze_trades/main.go
@@ -7,23 +7,44 @@ import (
 	"time"
 
 	"gocryptotrader/config"
-	"gocryptotrader/currency/coinmarketcap"
-	"gocryptotrader/engine"
+	"gocryptotrader/core"
 	"gocryptotrader/portfolio/analyze"
+
+	"github.com/urfave/cli/v2"
 )
 
-type Syncer struct {
-	bot *engine.Engine
-	cmc *coinmarketcap.Coinmarketcap
-}
+var (
+	app = &cli.App{
+		Name:                 "analyze",
+		Version:              core.Version(false),
+		EnableBashCompletion: true,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "command",
+				Value:       "",
+				Usage:       "command to run",
+				Destination: &command,
+			},
+		},
+	}
+	workingDir string
+	configFile string
+	verbose    bool
+	command    string
+)
 
 func main() {
-	wd, err := os.Getwd()
+	// flag.StringVar(&command, "command", "", "command to run status|up|up-by-one|up-to|down|create")
+	// flag.Parse()
+
+	err := app.Run(os.Args)
+
+	workingDir, err := os.Getwd()
 	if err != nil {
 		fmt.Printf("Could not get working directory. Error: %v.\n", err)
 		os.Exit(1)
 	}
-	configPath := filepath.Join(wd, "../confs/dev/backtest.json")
+	configPath := filepath.Join(workingDir, "../confs/dev/backtest.json")
 	cfg, err := config.ReadConfigFromFile(configPath)
 	if err != nil {
 		fmt.Printf("Could not read config. Error: %v. Path: %s\n", err, configPath)
@@ -33,23 +54,35 @@ func main() {
 	pf := &analyze.PortfolioAnalysis{
 		Config: cfg,
 	}
+
+	if command == "" {
+		fmt.Println("no command given")
+		return
+	} else if command == "pf" {
+		analyzePF(pf)
+		return
+	} else if command == "weights" {
+		pf.Analyze("")
+		prodWeighted := filepath.Join(workingDir, "../confs/prod.strat")
+		fmt.Println("saving", len(pf.Weights.Strategies), "pf weights to", prodWeighted)
+		pf.Weights.Save(prodWeighted)
+	} else if command == "store_all" {
+		allPath := filepath.Join(workingDir, "../confs/dev/strategy/all.strat")
+		fmt.Println("saving all.strat to", allPath)
+		pf.SaveAllStrategiesConfigFile(allPath)
+	}
+}
+
+func analyzePF(pf *analyze.PortfolioAnalysis) (err error) {
 	err = pf.Analyze("")
 	if err != nil {
 		fmt.Println("error analyzeTrades", err)
 	}
-
 	filename := fmt.Sprintf(
 		"portfolio_analysis_%v.json",
 		time.Now().Format("2006-01-02-15-04-05"))
-	filename = filepath.Join(wd, "results", filename)
+	filename = filepath.Join(workingDir, "results", filename)
 	fmt.Println("saved portfolio analysis to")
 	fmt.Println(filename)
-	pf.Save(filename)
-
-	// prodWeighted := filepath.Join(wd, "../confs/prod.strat")
-	// fmt.Println("saving", len(pf.Weights.Strategies), "pf weights to", prodWeighted)
-	// pf.Weights.Save(prodWeighted)
-	//
-	// allPath := filepath.Join(wd, "../confs/dev/strategy/all.strat")
-	// pf.SaveAllStrategiesConfigFile(allPath)
+	return pf.Save(filename)
 }
